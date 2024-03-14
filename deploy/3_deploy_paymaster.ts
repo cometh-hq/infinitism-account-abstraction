a1071a40ac6818e705b191fc7c2b5b0a0200b97a
Frederic DE MATOS <frederic@cometh.io>
2024-03-14 15:35:05 +0100

    Add verifying paymaster deployment script

---
+++ deploy/3_deploy_paymaster.ts
@@ -0,0 +1,42 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+
+const PAYMASTER_DEPOSIT = ethers.utils.parseEther("0.2");
+
+const deployPaymaster: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  const signer = hre.ethers.provider.getSigner();
+  const from = await signer.getAddress();
+
+  const { paymasterOwner } = await hre.getNamedAccounts();
+
+  const entryPointAddress = process.env.ENTRY_POINT_ADDRESS;
+
+  console.log(
+    "signer: ",
+    await signer.getAddress(),
+    "from: ",
+    from,
+    "paymasterOwner: ",
+    paymasterOwner,
+    "entrypoint: ",
+    entryPointAddress
+  );
+
+  const paymaster = await hre.deployments.deploy("VerifyingPaymaster", {
+    from,
+    args: [entryPointAddress, paymasterOwner],
+  });
+  console.log("==paymaster addr=", paymaster.address);
+
+  const paymasterContract = (
+    await hre.ethers.getContractAt("VerifyingPaymaster", paymaster.address)
+  ).connect(signer);
+  const tx = await paymasterContract.deposit({ value: PAYMASTER_DEPOSIT });
+  await tx.wait();
+  console.log("Paymaster deposited");
+};
+
+export default deployPaymaster;
