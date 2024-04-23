a17580e22f7ea5a2a3023ae8e8d0da608563a0b0
Frederic DE MATOS <frederic@cometh.io>
2024-04-23 15:41:46 +0200

    Add script to verify everything is correct with Connect API

--- scripts/paymaster_from_connect_api.ts
+++ scripts/paymaster_from_connect_api.ts
@@ -1,13 +1,42 @@
 import { Wallet } from "ethers";
 import { ethers } from "hardhat";
 import { fillAndSign } from "../test/UserOp";
-import { arrayify, defaultAbiCoder, hexConcat } from "ethers/lib/utils";
+import { defaultAbiCoder, hexConcat, hexlify } from "ethers/lib/utils";
 import { EntryPoint__factory } from "../typechain";
 import { simulationResultCatch } from "../test/testutils";
+import axios from "axios";
+
+async function validatePaymaster(
+  userOp: any,
+  validUntil: string,
+  validAfter: string
+): Promise<any> {
+  const paymasterApi = axios.create({ baseURL: "http://localhost:3000" });
+
+  const body = {
+    userOp: userOp,
+    validUntil: validUntil,
+    validAfter: validAfter,
+  };
+
+  const response = await paymasterApi.post(
+    "/verifying-paymaster/validate",
+    body,
+    {
+      headers: {
+        apiKey: "ezaeaeazea",
+        "x-consumer-access": "public",
+        "x-consumer-groups": "connect",
+        "x-consumer-username": "650ae7583be9c36fad85c0e2",
+        "x-project-chain-id": "421614",
+      },
+    }
+  );
+  return response?.data;
+}
 
 async function main(): Promise<void> {
   const owner = new Wallet(process.env.PRIVATE_KEY!);
-  const paymasterSigner = new Wallet(process.env.PAYMASTER_OWNER_PRIVATE_KEY!);
 
   const ethersSigner = ethers.provider.getSigner();
   const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
@@ -46,21 +75,31 @@ async function main(): Promise<void> {
     entryPoint
   );
 
-  const hash = await paymaster.getHash(userOp1, VALID_UNTIL, VALID_AFTER);
-  console.log(hash);
+  console.log("USER OP", userOp1);
 
-  const sig = await paymasterSigner.signMessage(arrayify(hash));
+  const apiRes = await validatePaymaster(
+    {
+      sender: userOp1.sender,
+      nonce: hexlify(userOp1.nonce),
+      initCode: userOp1.initCode,
+      callData: userOp1.callData,
+      callGasLimit: userOp1.callGasLimit,
+      verificationGasLimit: userOp1.verificationGasLimit,
+      preVerificationGas: userOp1.preVerificationGas,
+      maxFeePerGas: hexlify(userOp1.maxFeePerGas),
+      maxPriorityFeePerGas: userOp1.maxPriorityFeePerGas,
+      paymasterAndData: userOp1.paymasterAndData,
+      signature: userOp1.signature,
+    },
+    VALID_UNTIL,
+    VALID_AFTER
+  );
+  console.log("API RESULT", apiRes.result.paymasterAndData);
+
   const userOp = await fillAndSign(
     {
       ...userOp1,
-      paymasterAndData: hexConcat([
-        paymaster.address,
-        defaultAbiCoder.encode(
-          ["uint48", "uint48"],
-          [VALID_UNTIL, VALID_AFTER]
-        ),
-        sig,
-      ]),
+      paymasterAndData: apiRes.result.paymasterAndData,
     },
     owner,
     entryPoint
